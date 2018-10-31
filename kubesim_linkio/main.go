03a54183401e9cf9ff3a2d77b2729fc22d88a690
Jerome Brette <jbrette@gmail.com>
2018-10-31 13:16:49 +0000

    Update to latest version of linkio
    Change-Id: I9a49ad19e8f0560f78aeb2b07fa09a6cbf8ac3b8
    (cherry picked from commit 9c4686ed5a8f7d1f591a2d91645ca79f384b851d)

--- kubesim_linkio/main.go
+++ kubesim_linkio/main.go
@@ -17,52 +17,55 @@ limitations under the License.
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"github.com/kubedge/kubesim_base/config"
 	"github.com/kubedge/kubesim_linkio/linkio"
+	"io"
 	"log"
+	"net"
 	"os"
 	"time"
-	"bytes"
-	"io"
 )
 
-func TestOne() {
-	// a dummy buffer full of zeros to send over the link
-	var y [1000]byte
-	buf := bytes.NewBuffer(y[:])
+func simulate(targeturl string, targetspeed linkio.Throughput) {
+	// Create a new link at targetspeed
+	link := linkio.NewLink(targetspeed)
 
-	lr := linkio.NewLink(30 /* kbps */).NewLinkReader(buf)
-	for {
-		var x [1024]byte
-		n, err := lr.Read(x[:])
-		if n != 0 { 
-                }
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Printf("err %s", err)
-		}
+	// Open a connection
+	conn, err := net.Dial("tcp", targeturl)
+	if err != nil {
+		// handle error:w
 	}
+
+	// Create a link reader/writer
+	linkReader := link.NewLinkReader(io.Reader(conn))
+	linkWriter := link.NewLinkWriter(io.Writer(conn))
+
+	// Use them as you would normally...
+	fmt.Fprintf(linkWriter, "GET / HTTP/1.0\r\n\r\n")
+	status, err := bufio.NewReader(linkReader).ReadString('\n')
+
+	log.Printf("simulation status[%s] err[%s]", status, err)
 }
 
-//arguments
-// arg1=demotype
-// arg2=demovalue
 func main() {
-	log.Printf("%s", "kubesim Linkio client is running")
-	demotype := os.Args[1]
-	demovalue := os.Args[2]
-	log.Printf("demotype=%s, demovalue=%s", demotype, demovalue)
+	log.Printf("%s", "kubesim Linkio is running")
+	maintargeturl := os.Args[1]
+	maintargetspeed := os.Args[2]
+	log.Printf("targeturl=%s, targetspeed=%s", maintargeturl, maintargetspeed)
 
 	var conf config.Configdata
 	conf.Config()
-	log.Printf("kubesim 5G NR client:  product_name=%s, product_type=%s, product_family=%s, product_release=%s, feature_set1=%s, feature_set2=%s",
+	log.Printf("kubesim linkio config:  product_name=%s, product_type=%s, product_family=%s, product_release=%s, feature_set1=%s, feature_set2=%s",
 		conf.Product_name, conf.Product_type, conf.Product_family, conf.Product_release, conf.Feature_set1, conf.Feature_set2)
 
+	targeturl := "google.com:80"
+	targetspeed := 512 * linkio.KilobitPerSecond
 	log.Printf("starting for loop")
 	for {
+		simulate(targeturl, targetspeed)
 		time.Sleep(15 * time.Second) //every 15 seconds
 	}
-	log.Printf("%s", "kubesim 5G NR client is exiting")
+	log.Printf("%s", "kubesim Linkio is exiting")
 }
--- linkio/linkio.go
+++ linkio/linkio.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// This package provides an io.Reader that returns data,
-// simulating a network connection of a certain speed.
+// Package linkio provides an io.Reader and io.Writer that
+// simulate a network connection of a certain speed.
 package linkio
 
 import (
@@ -11,10 +11,43 @@ import (
 	"time"
 )
 
+// Throughput represents the link speed as an int64 bits per second
+// count. The representation limits the largest representable throughput
+// to approximately 9223 petabits per second.
+type Throughput int64
+
+// Common throughputs.
+//
+// To count the number of units in a Duration, divide:
+//	kilobit := linkio.KilobitPerSecond
+//	fmt.Print(int64(kilobit/linkio.BitPerSecond)) // prints 1024
+//
+// To convert an integer number of units to a Throughput, multiply:
+//	megabits := 10
+//	fmt.Print(linkio.Throughput(megabits)*time.BitPerSecond) // prints 10s
+//
+const (
+	BitPerSecond      Throughput = 1
+	BytePerSecond                = 8 * BitPerSecond
+	KilobitPerSecond             = 1024 * BitPerSecond
+	KilobytePerSecond            = 1024 * BytePerSecond
+	MegabitPerSecond             = 1024 * KilobitPerSecond
+	MegabytePerSecond            = 1024 * KilobytePerSecond
+	GigabitPerSecond             = 1024 * MegabitPerSecond
+	GigabytePerSecond            = 1024 * MegabytePerSecond
+)
+
 // A LinkReader wraps an io.Reader, simulating reading from a
 // shared access link with a fixed maximum speed.
 type LinkReader struct {
 	r    io.Reader
+	link *Link
+}
+
+// A LinkWriter wraps an io.Writer, simulating writer to a
+// shared access link with a fixed maximum speed.
+type LinkWriter struct {
+	w    io.Writer
 	link *Link
 }
 
@@ -25,6 +58,7 @@ type LinkReader struct {
 // by Go when scheduling reads from a contested blocking channel.
 type Link struct {
 	in    chan linkRequest
+	out   chan linkRequest
 	speed int64 // nanosec per bit
 }
 
@@ -42,11 +76,18 @@ func (link *Link) NewLinkReader(r io.Reader) (s *LinkReader) {
 	return
 }
 
+// NewLinkWriter returns a LinkWriter that writes bytes to r,
+// simulating that they arrived from a shared link.
+func (link *Link) NewLinkWriter(w io.Writer) (s *LinkWriter) {
+	s = &LinkWriter{w: w, link: link}
+	return
+}
+
 // NewLink returns a new Link running at kbps.
-func NewLink(kbps int) (l *Link) {
+func NewLink(throughput Throughput) (l *Link) {
 	// allow up to 100 outstanding requests
-	l = &Link{in: make(chan linkRequest, 100)}
-	_ = l.SetSpeed(kbps)
+	l = &Link{in: make(chan linkRequest, 100), out: make(chan linkRequest, 100)}
+	l.SetThroughput(throughput)
 
 	// This goroutine serializes the requests. He could calculate
 	// link utilization by comparing the time he sleeps waiting for
@@ -61,17 +102,22 @@ func NewLink(kbps int) (l *Link) {
 			lr.done <- true
 		}
 	}()
+	go func() {
+		for lr := range l.out {
+			// bits * nanosec/bit = nano to wait
+			delay := time.Duration(int64(lr.bytes*8) * l.speed)
+			time.Sleep(delay)
+			lr.done <- true
+		}
+	}()
 
 	return
 }
 
-// SetSpeed set current speed of the bytes returned, returning the old speed.
-// The speed is expressed in kilobits per second, where kilo = 1024.
-func (l *Link) SetSpeed(kbps int) int {
-	old := 1e9 * l.speed / 1024
-	// l.speed is stored in ns/bit
-	l.speed = 1e9 / int64(kbps*1024)
-	return int(old)
+// SetThroughput sets the current link throughput
+func (link *Link) SetThroughput(throughput Throughput) {
+	// link.speed is stored in ns/bit
+	link.speed = 1e9 / int64(throughput)
 }
 
 // why isn't this in package math? hmm.
@@ -90,6 +136,26 @@ func (l *LinkReader) Read(buf []byte) (n int, err error) {
 	// size.
 	toRead := min(len(buf), 1500)
 	n, err = l.r.Read(buf[0:toRead])
+	if err != nil {
+		return 0, err
+	}
+
+	// send in the request to sleep to the Link and sleep
+	lr := linkRequest{bytes: n, done: make(chan bool)}
+	l.link.in <- lr
+	_ = <-lr.done
+
+	return
+}
+
+// Satisfies interface io.Writer.
+func (l *LinkWriter) Write(buf []byte) (n int, err error) {
+	// Write small chunks at a time, even if they attempt more,
+	// preventing one LinkReader from saturating the simulated link.
+	// 1500 is the MTU for Ethernet, i.e. a likely maximum packet
+	// size.
+	toWrite := min(len(buf), 1500)
+	n, err = l.w.Write(buf[0:toWrite])
 	if err != nil {
 		return 0, err
 	}
